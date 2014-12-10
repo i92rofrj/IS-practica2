cbf62202e0fc36e18293dc813e0e0236dbdda20e
David Sanchez Fernandez <i22safed@uco.es>
2014-12-10 19:12:53 +0100

    Wesel

--- Dentista/src/Dentista.cpp
+++ Dentista/src/Dentista.cpp
@@ -13,7 +13,7 @@ int main() {
 	cout << "Esto es una prueba de pushion"
 <<<<<<< HEAD
 	cout << "cambiando algo" << endl; // prints !!!Hello World!!!
-	cout << "PitoPitoPito" << endl;
+	cout << "WeselWeselWesel" << endl;
 =======
 	cout << "cambiando salgo" << endl; // prints !!!Hello World!!!
 >>>>>>> f990482cf1d498e0e061a467165affc6ba7d580b
