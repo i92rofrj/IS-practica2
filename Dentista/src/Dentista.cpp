f0aee323bf9d8c8d9de00ad8dc07868503d63aa7
Jesús Rodríguez Freire <i92rofrj@uco.es>
2014-12-17 18:49:53 +0100

    Añadido comentario

--- Dentista/src/Dentista.cpp
+++ Dentista/src/Dentista.cpp
@@ -10,12 +10,13 @@
 using namespace std;
 
 int main() {
-	cout << "Esto es una prueba de pushion"
-<<<<<<< HEAD
-	cout << "cambiando algo" << endl; // prints !!!Hello World!!!
-	cout << "WeselWeselWesel" << endl;
-=======
-	cout << "cambiando salgo" << endl; // prints !!!Hello World!!!
->>>>>>> f990482cf1d498e0e061a467165affc6ba7d580b
-	return 0;
+/**
+ * Para subir los cambios
+ * Git add nombreFichero
+ * Git commit -m "Comentario de cambio"
+ * Git push
+ *
+ * Para traerte los cambios
+ * Git pull
+ */
 }
